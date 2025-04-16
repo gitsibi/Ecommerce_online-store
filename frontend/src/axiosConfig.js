d56740d95482612259e41f56729b73ce64ed7451
Jananisibi <jananisibi2002@gmail.com>
2025-04-16 15:05:16 +0530

    completed full project

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,8 +1,17 @@
+// import axios from 'axios';
+ 
+//  const instance = axios.create({
+//      baseURL: 'http://localhost:8000', // your server
+//      withCredentials: true, // crucial for sending cookies
+//  });
+ 
+//  export default instance;
+
 import axios from 'axios';
- 
- const instance = axios.create({
-     baseURL: 'http://localhost:8000', // your server
-     withCredentials: true, // crucial for sending cookies
- });
- 
- export default instance;
+
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+});
+
+export default instance;
