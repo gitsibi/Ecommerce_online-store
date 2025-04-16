a2bfcfa3281a675a62b0e28d986da4dd716a5d0c
Jananisibi <jananisibi2002@gmail.com>
2025-04-16 16:43:17 +0530

    completed full project

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,8 @@ const allowedOrigins = [
   'http://localhost:5173', // Local development
   'https://transcendent-clafoutis-c78124.netlify.app', // Production site 1
   'https://glowing-ganache-83cc2c.netlify.app', // Production site 2 (add this origin)
-  'https://calm-treacle-f81b77.netlify.app' // newly added
+  'https://calm-treacle-f81b77.netlify.app', // newly added
+   'https://nimble-sfogliatella-270dc3.netlify.app'
 
 ];
 
