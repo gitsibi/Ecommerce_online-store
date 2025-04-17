38b155eba7b11b6b36689adcb73299c9d3da0d37
Jananisibi <jananisibi2002@gmail.com>
2025-04-17 13:20:24 +0530

    completed full project

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const orders = require('./controller/orders');
 
 const allowedOrigins = [
   'http://localhost:5173', // Local development
-   'https://ecommerce-online-frontend.onrender.com'
+   'https://ecommerce-online-store-frontend.onrender.com'
 
 ];
 const corsOptions = {
